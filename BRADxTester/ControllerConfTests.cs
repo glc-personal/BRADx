d8065d910802b0c09bdcab942c420709b9c0a23e
glc-personal <gabriel.lopez.candales@gmail.com>
2025-08-18 08:40:04 -0700

    Added an additional test for verifying the loaded configs.

--- BRADxTester/ControllerConfTests.cs
+++ BRADxTester/ControllerConfTests.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using Configurations;
 using Configurations.Helpers;
 
@@ -13,13 +14,47 @@ public class ControllerConfTests
         _controllerConfig = new ControllerConfig();
     }
 
-    //[TestCase("C:\\Users\\gabri\\RiderProjects\\BRADx\\BRADx\\bin\\Debug\\net8.0\\configs\\ics.reader.xml")]
+    [TestCase("C:\\Users\\gabri\\RiderProjects\\BRADx\\BRADx\\bin\\Debug\\net8.0\\configs\\ics.reader.xml")]
     [TestCase("C:\\Users\\gabri\\RiderProjects\\BRADx\\BRADx\\bin\\Debug\\net8.0\\configs\\ics.deck.xml")]
     public void LoadControllerConfigTest(string configXmlPath)
     {
         try
         {
             var controllerConfig = ControllerConfigXmlLoader.Load(configXmlPath);
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail(ex.Message);
+        }
+    }
+
+    [TestCase("C:\\Users\\gabri\\RiderProjects\\BRADx\\BRADx\\bin\\Debug\\net8.0\\configs\\ics.reader.xml")]
+    [TestCase("C:\\Users\\gabri\\RiderProjects\\BRADx\\BRADx\\bin\\Debug\\net8.0\\configs\\ics.deck.xml")]
+    public void VerifyLoadedControllerConfigRootTest(string configXmlPath)
+    {
+        try
+        {
+            var controllerConfig = ControllerConfigXmlLoader.Load(configXmlPath);
+            
+            // verify a few things
+            var controllerName = controllerConfig.Name;
+            var controllerDescription = controllerConfig.Description;
+            var controllerDisplayName = controllerConfig.DisplayName;
+            
+            var xmlDoc = XDocument.Load(configXmlPath);
+            var root = xmlDoc.Root;
+            if (root != null)
+            {
+                var name = root.Attribute("name")?.Value;
+                Assert.That(name, Is.EqualTo(controllerName));
+                var description = root.Attribute("description")?.Value;
+                Assert.That(description, Is.EqualTo(controllerDescription));
+                var displayName = root.Element("displayName")?.Value;
+                if (displayName != null)
+                {
+                    Assert.That(displayName, Is.EqualTo(controllerDisplayName));
+                }
+            }
         }
         catch (Exception ex)
         {
